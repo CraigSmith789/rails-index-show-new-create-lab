9e9d4e5f2e68441b5a2041f87a1dcdbea454eb3a
CraigSmith789 <craig789@gmail.com>
2020-12-29 14:40:24 -0500

    commit tree 25595ee61705a75c44581d49ce2288de7ba354ab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+ resources :coupons, only: [:index, :new, :create]
+ get '/coupon/:id', to: 'coupons#show', as: 'coupon'
+ 
+end
+  # get 'coupons/index'
+
+  # get 'coupons/show'
+
+  # get 'coupons/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +63,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
